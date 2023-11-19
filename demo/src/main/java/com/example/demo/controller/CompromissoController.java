c5e6e4c144bb022eefbaa1dac9b6586ac5585187
b0rb44 <dennerdeborba@gmail.com>
2023-11-18 22:58:14 -0300

    add compromissocontroller

--- demo/src/main/java/com/example/demo/controller/CompromissoController.java
+++ demo/src/main/java/com/example/demo/controller/CompromissoController.java
@@ -1,5 +1,87 @@
 package com.example.demo.controller;
 
-public class CompromissoController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Compromisso;
+import com.example.demo.repository.CompromissoRepository;
+
+
+@RestController
+@RequestMapping("/compromisso")
+public class CompromissoController {
+	@Autowired
+	CompromissoRepository repo;
+	
+	@GetMapping()
+	public ResponseEntity<List<Compromisso>>getCompromisso(){
+		return ResponseEntity.status(HttpStatus.OK).body(repo.findAll());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Compromisso> inserirCompromisso(@RequestBody Compromisso compromisso){
+		Compromisso ct = repo.save(compromisso);
+		return ResponseEntity.status(HttpStatus.CREATED).body(ct);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Compromisso> alterarCompromisso(@PathVariable("id") Long idcompromisso,
+			@RequestBody Compromisso compromisso){
+		Optional<Compromisso> opCompromisso = repo.findById(idcompromisso);
+				try {
+					Compromisso ct = opCompromisso.get();
+					ct.setLocalcomp(compromisso.getLocalcomp());
+					ct.setContato(compromisso.getContato());
+					ct.setDatacomp(compromisso.getDatacomp());
+					ct.setHora(compromisso.getHora());
+					ct.setStatus(compromisso.getStatus());
+					ct.setIdcontato(compromisso.getIdcontato());
+					repo.save(ct);
+					return ResponseEntity.status(HttpStatus.OK).body(ct);
+				}
+		catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Compromisso>getUmCompromisso(@PathVariable("id")Long id){
+		Optional<Compromisso> opCompromisso = repo.findById(id);
+		try {
+			Compromisso ct = opCompromisso.get();
+			return ResponseEntity.status(HttpStatus.OK).body(ct);
+		}
+		catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+	
+		
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Compromisso>DeletarUmCompromisso(@PathVariable("id")Long id){
+		Optional<Compromisso> opCompromisso = repo.findById(id);
+		try {
+			Compromisso ct = opCompromisso.get();
+			repo.delete(ct);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ct);
+		}
+		catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+	
+	
 }
--- demo/src/main/resources/import.sql
+++ demo/src/main/resources/import.sql
@@ -1,4 +1,5 @@
 insert into tb_contatos(nome, email)values('maria','maria@gmail.com');
 insert into tb_contatos(nome, email)values('joao','joao@gmail.com');
 insert into tb_contatos(nome, email)values('pedro','pedro@gmail.com');
-insert into tb_compromisso(localcomp,contato,datacomp,hora,status,idcontato)values('Blumenau','9999','16/11/2023','15:00:00','Em aberto','1');
+insert into tb_compromisso(localcomp,contato,datacomp,hora,status,idcontato)values('São Paulo','888','20/11/2023','21:00:00','Finalizado','1');
+insert into tb_compromisso(localcomp,contato,datacomp,hora,status,idcontato)values('Blumenau','123','25/12/2023','11:00:00','Cancelado','2');
